72648f97db8b1126463babbd6698dc6f5969e264
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2020-10-06 13:26:39 -0300

    validação do login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 // Components
 import Button from '../../components/Button';
 import Input from '../../components/Input';
-import { Form } from '@unform/web';
+
+// Utils
+import getValidationErrors from '../../utils/getValidationErrors';
 
 // Assets
 import logo from '../../assets/images/logo.svg';
@@ -14,16 +20,38 @@ import logo from '../../assets/images/logo.svg';
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-  function handleSubmit(data: object): void{
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('O campo Email é obrigatório')
+          .email('Digite um email válido'),
+        password: Yup.string()
+          .required('O campo Senha é obrigatório'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false
+      });
+
+    } catch (err) {
+      console.log(err);
+
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
+    }
+  },[]);
 
   return (
     <Container>
       <Content>
         <img src={logo} alt="GoBarber"/>
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
           <Input name="email" icon={FiMail} placeholder="Email"/>
 
